16a689366944c32bdf202a61c4d529d9f53145f2
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-06-04 09:41:04 +0900

    Update ignore.js

--- commands/ignore.js
+++ commands/ignore.js
@@ -1,44 +1,49 @@
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const {Permissions} = require("discord.js");
 module.exports = {
-  name: 'ignore',
-  description: '読み上げ時に無視する接頭文字列を設定します。',
-  clientPermissions: ['ADMINISTRATOR'],
-  execute(message, args) {
+  data: new SlashCommandBuilder()
+      .setName('ignore')
+      .setDescription('読み上げ時に無視する接頭文字列を設定')
+      .addSubcommand(subcommand =>
+          subcommand.setName('add')
+              .setDescription('無視する接頭語を追加')
+              .addStringOption(option =>
+                  option.setName('target')
+                      .setDescription('無視する接頭語')
+                      .setRequired(true)
+              )
+      )
+      .addSubcommand(subcommand =>
+          subcommand.setName('remove')
+              .setDescription('無視する接頭語を削除')
+              .addStringOption(option =>
+                  option.setName('target')
+                      .setDescription('削除する接頭語')
+                      .setRequired(true)
+              )
+      ),
+  async execute(interaction) {
 
-    if (!message.member.hasPermission('ADMINISTRATOR')) {
-      console.log("[ignore command] blocked operation.");
-      return;
-    }
+    if (!interaction.member.has(Permissions.FLAGS.ADMINISTRATOR)) { return; }
 
-    const guild = message.guild;
+    const guild = interaction.guild;
     const file = require('../modules/file.js');
     const path = "servers/" + guild.id + "/read/ignore.json";
-    var ignoreList = file.readJSONSync(path, []);
-    if (args[0] === "add") {
-      args.shift();
-      for (var i = 0; i < args.length; i++) {
-        ignoreList.push(args[i]);
-      }
+    let ignoreList = file.readJSONSync(path, []);
+
+    const subcommand = interaction.options.getSubcommand();
+    const target = interaction.options.getString('target');
+
+    if (subcommand === "add") {
+      ignoreList.push(target);
       ignoreList = Array.from(new Set(ignoreList));
-    } else if (args[0] === "remove") {
-      args.shift();
-      for (var i = 0; i < args.length; i++) {
-        ignoreList = ignoreList.filter(item => item !== args[i]);
-      }
+    } else if (subcommand === "remove") {
+      ignoreList = ignoreList.filter(item => item !== target);
     }
-    var commands = '';
-    for (var i = 0; i < ignoreList.length; i++) {
-      if (i === ignoreList.length - 1) {
-        commands += "`" + ignoreList[i] + "`";
-      } else {
-        commands += "`" + ignoreList[i] + "`, ";
-      }
-    }
-    message.channel.send({
-      embed: {
-        title: "読み上げ無視設定",
-        description: "読み上げコマンド実行時次の単語から始まるメッセージはすべて無視されます。\n" + commands
-      }
-    });
+
+    let commands = ignoreList.join(',');
+
+    interaction.reply({ embed: { title: "読み上げ無視設定", description: "読み上げコマンド実行時次の単語から始まるメッセージはすべて無視されます。\n" + commands } });
     file.writeJSONSync(path, ignoreList);
   }
 }
