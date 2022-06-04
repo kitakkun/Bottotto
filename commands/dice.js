56b2e98620b709f1714cfa35270d52005d889b81
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-06-04 09:14:44 +0900

    remove subcommand from dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -2,42 +2,24 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
+  help: new MessageEmbed()
+      .setTitle('diceコマンドの使い方')
+      .setDescription('ダイスを振ります第1引数にはダイスの数を、第2引数にはダイスの最大の目を入力してください。'),
   data: new SlashCommandBuilder()
       .setName('dice')
       .setDescription('ダイスを振ります。TRPGで使えます。')
-      .addSubcommand(subcommand =>
-        subcommand.setName('h')
-            .setDescription('使い方を表示')
+      .addNumberOption(option =>
+          option.setName('diceCounts')
+              .setDescription('振るダイスの数')
+              .setRequired(true)
       )
-      .addSubcommand(subcommand =>
-          subcommand.setName('roll')
-              .setDescription('ダイスを振ります')
-              .addNumberOption(option =>
-                  option.setName('diceCounts')
-                      .setDescription('振るダイスの数')
-                      .setRequired(true)
-              )
-              .addNumberOption(option =>
-                  option.setName('diceMax')
-                      .setDescription('ダイスの最大の目')
-                      .setRequired(true)
-              )
+      .addNumberOption(option =>
+          option.setName('diceMax')
+              .setDescription('ダイスの最大の目')
+              .setRequired(true)
       )
   ,
   async execute(interaction) {
-    const subcommand = interaction.options.getSubcommand();
-
-    if (subcommand === 'h')
-    {
-      const message = new MessageEmbed()
-          .setTitle('diceコマンドの使い方')
-          .setDescription('ダイスを振ります第1引数にはダイスの数を、第2引数にはダイスの最大の目を入力してください。')
-      interaction.reply({ embeds: { message } });
-      return;
-    }
-
-    if (subcommand === 'roll')
-    {
 
       let num = interaction.options.getNumber('diceCounts');
       let max = interaction.options.getNumber('diceMax');
@@ -58,6 +40,5 @@ module.exports = {
       function getRandInt(min, max) {
         return Math.floor(Math.random() * max) + min;
       }
-    }
   }
 }
