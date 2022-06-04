7502cae1fdcc011591dabce41a008b5c481c5256
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-06-04 09:44:17 +0900

    Update wr.js

--- commands/wr.js
+++ commands/wr.js
@@ -1,11 +1,17 @@
+const {SlashCommandBuilder} = require("@discordjs/builders");
 module.exports = {
-  name: 'wr',
-  description: 'wolframへのURLを生成します。',
-  execute(message, args) {
-    for (var i = 0; i < args.length; i++) {
-      args[i] = encodeURIComponent(args[i])
-    }
-    output = "Wolfram大先生の解答はこちらです⇒ https://ja.wolframalpha.com/input/?i=" + args.join('+');
-    message.channel.send(output).then(ms => ms.suppressEmbeds(true));
+  data: new SlashCommandBuilder()
+      .setName('wr')
+      .setDescription('wolframへのURLを生成します。')
+      .addStringOption(option =>
+          option.setName('expression')
+              .setDescription('解釈する数式')
+              .setRequired(true)
+      ),
+  async execute(interaction) {
+    let expression = interaction.options.getString('expression');
+    expression = expression.replaceAll(' ', '+');
+    let output = "Wolfram大先生の解答はこちらです⇒ https://ja.wolframalpha.com/input/?i=" + expression;
+    interaction.reply(output).then(ms => ms.suppressEmbeds(true));
   }
 }
