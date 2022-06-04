130d408a616f3182fbbeb3d824b53c82d47b0d7a
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-06-04 09:50:42 +0900

    Update h.js

--- commands/h.js
+++ commands/h.js
@@ -1,24 +1,45 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
 module.exports = {
-  name: 'h',
-  description: '利用可能なコマンドの一覧を表示します。',
-  execute(message, args) {
+  data: new SlashCommandBuilder()
+      .setName('h')
+      .setDescription('コマンドのヘルプ')
+      .addStringOption(option =>
+          option.setName('commandName')
+              .setDescription('コマンド名')
+      )
+      .addStringOption(option =>
+          option.setName('subcommandName')
+              .setDescription('サブコマンド名')
+      )
+  ,
+  async execute(interaction) {
+
+    const commandName = interaction.options.getString('commandName');
+    const subcommandName = interaction.options.getString('subcommandName');
+
     const fs = require('fs');
     const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-    var output = "コマンドプレフィックス「k!」に続けて実行できるコマンドを以下にリスト表示します。\n\n";
+    if (commandName)
+    {
+      const command = require(`./${commandName}.js`);
+      if (command) interaction.reply("Command not found");
+      if (subcommandName) {
+        const subcommand = command.data.options.find(i => i.name === subcommandName);
+        await interaction.reply(subcommand.name + "\n" + subcommand.name.description);
+      } else {
+        interaction.reply(command.data.name + "\n" + command.name.description);
+      }
+    }
 
+    let output = '';
     for (const file of commandFiles) {
       const command = require(`./${file}`);
-      const permission = command.clientPermissions;
-      if (permission != null && permission.includes('ADMINISTRATOR')) {
-        continue;
-      }
       output += "`" + command.name + "` " + command.description + "\n";
     }
 
-    output += "\n各コマンドの具体的な使用方法に関しては、「k!コマンド名 h」と入力してみてください。";
-
-    message.channel.send({
+    interaction.reply({
       embed: {
         title: "コマンド一覧",
         description: output
