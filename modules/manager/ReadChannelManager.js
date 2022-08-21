9c1f6cceb356499e802ba889977cdfff3e45a89f
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-08-21 14:40:35 +0900

    resolve roles, usernames, text channels, and ignore emoji

--- modules/manager/ReadChannelManager.js
+++ modules/manager/ReadChannelManager.js
@@ -20,11 +20,72 @@ module.exports.ReadChannelManager = class ReadChannelManager {
         this.isPlaying = false;
     }
 
-    #modify_text(text) {
-        // ignore urls
-        text = text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
-        // resolve role names
+    /**
+     * format text to read aloud
+     * @param text input text
+     * @param guild guild object
+     * @returns {*} formatted text
+     */
+    #format_text(text, guild) {
+        text = this.#ignore_url(text);
+        text = this.#ignore_emoji(text);
+        text = this.#resolve_role_id(text, guild);
+        text = this.#resolve_member_id(text, guild);
+        text = this.#resolve_text_channel_id(text, guild);
+        return text;
+    }
+
+    // ignore urls
+    #ignore_url(text) {
+        return text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
+    }
+
+    #ignore_emoji(text) {
+        text = text.replaceAll('<:[a-zA-Z0-9_]+:[0-9]+>', '');
         return text
+    }
+
+    // resolve role names
+    #resolve_role_id(text, guild) {
+        let roleIDs = text.match(/<@&\d{18}>/);
+        if (roleIDs != null) {
+            roleIDs = Array.from(roleIDs);
+            const roles = guild.roles;
+            for (let i = 0; i < roleIDs.length; i++) {
+                const roleID = roleIDs[i].replace(/\D/g, '');
+                const role = roles.cache.find(role => role.id === roleID);
+                if (role != null) text = text.replaceAll(roleIDs[i], "@" + role.name);
+            }
+        }
+        return text;
+    }
+
+    #resolve_member_id(text, guild) {
+        let memberIDs = text.match(/<@\d{18}>/);
+        if (memberIDs != null) {
+            memberIDs = Array.from(memberIDs);
+            const members = guild.members;
+            for (let i = 0; i < memberIDs.length; i++) {
+                const memberID = memberIDs[i].replace(/\D/g, '');
+                const member = members.cache.find(member => member.id === memberID);
+                if (member != null) text = text.replaceAll(memberIDs[i], "@" + member.displayName);
+            }
+        }
+        return text;
+    }
+
+    #resolve_text_channel_id(text, guild) {
+        let channelIDs = text.match(/<#\d{18}>/);
+        if (channelIDs != null) {
+            channelIDs = Array.from(channelIDs);
+            const channels = guild.channels;
+            for (let i = 0; i < channelIDs.length; i++) {
+                const channelID = channelIDs[i].replace(/\D/g, '');
+                const channel = channels.cache.find(channel => channel.id === channelID);
+                if (channel != null) text = text.replaceAll(channelIDs[i], "テキストチャンネル" + channel.name);
+            }
+        }
+        return text;
     }
 
     async speech(message) {
@@ -48,7 +109,7 @@ module.exports.ReadChannelManager = class ReadChannelManager {
         const wav_filename = `temp.wav`;
 
         let text = message.content;
-        text = this.#modify_text(text)
+        text = this.#format_text(text, message.guild)
 
         // construct command string.
         const command = `echo "${text}" | \ 
